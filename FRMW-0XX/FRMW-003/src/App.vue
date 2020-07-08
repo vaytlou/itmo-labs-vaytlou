de412dd92b0107eb2ec1715104ef9be328f6846b
vaytlou <36851964+vaytlou@users.noreply.github.com>
2020-07-08 21:05:17 +0300

    Add files via upload

---
+++ FRMW-0XX/FRMW-003/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <main id="app">
+        <h3>Список задач</h3>
+        <List/>
+    </main>
+</template>
+
+<script>
+    import List from './List.vue'
+    export default {
+        name: 'app',
+        components: {
+            List
+        }
+    }
+</script>
+
+<style>
+    body {
+        /* font-family: Karla, sans-serif; */
+        font-size: 16px;
+        margin: 0;
+    }
+    #app {
+        max-width: 510px;
+        margin: 50px auto;
+        padding: 0 10px;
+    }
+</style>
