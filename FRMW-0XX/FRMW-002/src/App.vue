bd40779903eb4c9ce04998a53c75686e1975641f
vaytlou <36851964+vaytlou@users.noreply.github.com>
2020-07-08 20:23:57 +0300

    Add files via upload

---
+++ FRMW-0XX/FRMW-002/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <main id="app">
+    <h2>Капитонов Евгений</h2>
+    <h3>Vue</h3>
+    <binding/>
+  </main>
+</template>
+
+<script>
+  import binding from './binding.vue'
+  export default {
+    name: 'app',
+    components: {
+      binding
+    }
+  }
+</script>?
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial;
+    text-align: center;
+    margin-top: 5%;
+  }
+</style>
---
+++ FRMW-0XX/FRMW-002/src/binding.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input v-model="message" placeholder="Hey!" class="button fit small" />
+        <input v-model="message" placeholder="Hey!" class="button fit small" />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            message: String
+        }
+    }
+</script>
+
+<style>
+    input {
+        width: 210px;
+        padding: 0.5em;
+        margin: 0.5em;
+        color: black;
+        background: white;
+        border: 1px solid black;
+        border-radius: 3px;
+    }
+    input:focus {
+        border: 1px solid black;
+    }
+</style>
