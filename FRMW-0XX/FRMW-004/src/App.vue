1f3b5463f299dd229eb588dbc9569f15f1055cfa
vaytlou <36851964+vaytlou@users.noreply.github.com>
2020-07-08 22:04:10 +0300

    Add files via upload

---
+++ FRMW-0XX/FRMW-004/src/App.vue
@@ -0,0 +1,23 @@
+  <template>
+    <main id="app">
+        <h3>Vue</h3>
+        <Step/>
+    </main>
+</template>
+
+<script>
+    import Step from './Step.vue'
+    export default {
+        name: 'app',
+        components: {
+            Step
+        }
+    }
+</script>
+
+<style>
+    main {
+        width: 600px;
+        margin: auto;
+    }
+</style>
---
+++ FRMW-0XX/FRMW-004/src/Step.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <button @click="inc() + upd()">⬆︎</button>
+        <input type="text" :placeholder='[[ counter ]]' v-model.number="counter" v-on:keyup="upd" @change="upd"/>
+        <div v-for="(value) in list" v-bind:key="value">
+            <h3>
+                <span>{{2}}</span>
+                <sup>{{ value }}</sup>= {{2 ** value}}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: Math.floor(Math.PI),
+                list: [0, 1, 2]
+            };
+        },
+        methods: {
+            inc() {
+                this.counter++;
+            },
+            upd() {
+                const id = this.counter + 1;
+                // const index = this.list.length;
+                this.list = Array.apply(null, {
+                    length: id
+                }).map(Number.call, Number);
+                console.log(this.list);
+            }
+        }
+    };
+</script>
